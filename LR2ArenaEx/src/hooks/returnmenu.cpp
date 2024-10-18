5525a77528a4f6f8c80698c77b654e283b7ca484
Sayaka <alice@lolita.technology>
2024-10-19 01:30:06 +0200

    fix condition to send escape, hide graph on escape

--- LR2ArenaEx/src/hooks/returnmenu.cpp
+++ LR2ArenaEx/src/hooks/returnmenu.cpp
@@ -2,6 +2,7 @@
 #include <client/client.h>
 #include <network/enums.h>
 #include <iostream>
+#include <gui/graph.h>
 
 #include "returnmenu.h"
 #include "maxscore.h"
@@ -10,10 +11,12 @@ void hkReturnMenu() {
 	std::cout << "[+] Returning to menu" << std::endl;
 	auto score = client::state.peers[client::state.remoteId].score;
 	// Do not send cancelled if pressing escape at the end of the chart (to show results faster)
-	if ((score.p_great + score.great + score.good + score.bad + score.poor) * 2 < hooks::max_score::maxScore && client::state.peers[client::state.remoteId].ready) {
-		hooks::return_menu::is_returning_to_menu = true;
-		client::Send(network::ClientToServer::CTS_CHART_CANCELLED, ""); // send escaped
+	if (((score.p_great + score.great + score.good + score.bad + score.poor) * 2 >= hooks::max_score::maxScore && client::state.peers[client::state.remoteId].ready)) {
+		return;
 	}
+	hooks::return_menu::is_returning_to_menu = true;
+	gui::graph::showGraph = false;
+	client::Send(network::ClientToServer::CTS_CHART_CANCELLED, ""); // send escaped
 }
 
 DWORD jmp_lr2body_419863 = 0x419863;
